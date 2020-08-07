a365a8492aedcee34f918265d39c878e6757d46e
S3N1CH <semenpeleskov@gmail.com>
2020-08-07 17:02:30 +0300

    Earth adapted for screens <576px

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var instance = new vidbg(
     mp4: "video/world.mp4", // URL or relative path to MP4 video
     webm: "video/world.webm", // URL or relative path to webm video
     poster: "video/poster.jpg", // URL or relative path to fallback image
-    overlay: true, // Boolean to display the overlay or not
+    overlay: false, // Boolean to display the overlay or not
     overlayColor: "#000", // The overlay color as a HEX
     overlayAlpha: 0.3, // The overlay alpha. Think of this as the last integer in RGBA()
   },
